63703d5f8c77d6f57a4ee7283301f37e7afa4e5a
Dunja55 <elguaje358@gmail.com>
2017-06-08 13:47:54 +0200

    instalacija

--- Services/e2e/app.po.ts
+++ Services/e2e/app.po.ts
@@ -1,6 +1,6 @@
-import { browser, by, element } from 'protractor';
+import { browser, element, by } from 'protractor';
 
-export class BookingAppClientPage {
+export class ServicesPage {
   navigateTo() {
     return browser.get('/');
   }
